2b2eebcbfce7841361f14c99d775ac2aa2be5238
totemtang <totem825@gmail.com>
2022-12-23 16:58:54 -0800

    Fix RedisGraph

--- test_scripts/runGraphBuildQuery.sh
+++ test_scripts/runGraphBuildQuery.sh
@@ -13,14 +13,16 @@ DATASET=$1
 TEST_APPROACH=$2
 FILE_NAME=all
 RUNS=3
-DEP_TYPE=("M" "L")
+DEP_TYPE="M"
+
+TEST_FILE_FOLDER=$TEST_HOME/dataset/graphbuild_top20/$DATASET
 
 for RUN in `seq 1 $RUNS`
 do
-    for j in "${!DEP_TYPE[@]}"
-    do
-      DEP=${DEP_TYPE[j]}
-      $TEST_HOME/runOneDepTest.sh $DATASET $FILE_NAME $TEST_APPROACH $DEP $RUN
-    done
+	for FULL_FILE_NAME in $TEST_FILE_FOLDER/*
+	do
+		FILE_NAME=$(basename "$FULL_FILE_NAME")
+		$TEST_HOME/runOneDepRefTest.sh $DATASET "$FILE_NAME" $TEST_APPROACH $DEP_TYPE $RUN
+	done	
 done
 
